4b13c53e24f3349b4b9d9be74f0c82219e5b9c20
Amalya syohiba <96401534+Amalyasyohiba11@users.noreply.github.com>
2022-11-29 10:30:02 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/latihanenkapsulasi2/LatihanEnkapsulasi2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.latihanenkapsulasi2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class LatihanEnkapsulasi2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
